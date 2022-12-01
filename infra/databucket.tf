e3f748e7d6ffe5157f6a505175c92afeb38a32ad
Dontosh <65369203+Dontosh@users.noreply.github.com>
2022-12-01 01:05:33 +0100

    changed bucket local name

--- infra/databucket.tf
+++ infra/databucket.tf
@@ -1,6 +1,6 @@
 # Jim; this just fails ... commented it out ! We need to figure this out later, starting new task instead...
 
-resource "aws_s3_bucket" "analyticsbucket" {
+resource "aws_s3_bucket" "analyticsbucket-1049" {
   bucket = "analytics-${var.candidate_id}"
 }
 // test
