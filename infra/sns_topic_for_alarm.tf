2827c45084def52bc7923946785dcf9a1571b441
Dontosh <65369203+Dontosh@users.noreply.github.com>
2022-12-01 18:16:10 +0100

    trying alarm

--- infra/sns_topic_for_alarm.tf
+++ infra/sns_topic_for_alarm.tf
@@ -1,3 +1,20 @@
+resource "aws_cloudwatch_metric_alarm" "more_than_five_carts" {
+  alarm_name                = "carts-more-than-five-1049"
+  namespace                 = "1049"
+  metric_name               = "carts.value"
+
+  comparison_operator       = "GreaterThanThreshold"
+  threshold                 = "5"
+  evaluation_periods        = "3"
+  period                    = "300"
+
+  statistic                 = "Maximum"
+
+  alarm_description         = "This alarm goes off as soon as the number of carts exceeds 5"
+  insufficient_data_actions = []
+  alarm_actions       = [aws_sns_topic.user_updates.arn]
+}
+
 resource "aws_sns_topic" "alarms" {
   name = "alarm-topic-${var.candidate_id}"
 }
