223da14c01d9fbf15121cec49bcea88fe768f1dd
Dontosh <65369203+Dontosh@users.noreply.github.com>
2022-12-01 14:57:02 +0100

    changes

--- src/main/java/no/shoppifly/ShoppingCartController.java
+++ src/main/java/no/shoppifly/ShoppingCartController.java
@@ -85,6 +85,6 @@ public class ShoppingCartController implements ApplicationListener<ApplicationRe
         Gauge.builder("carts", cartService,
                 cartService -> cartService.getAllCarts().size()).register(meterRegistry);
         Gauge.builder("cartsvalue", naiveCart, NaiveCartImpl::total).register(meterRegistry);
-        Gauge.builder("checkouts", numberOfCheckouts, Integer::doubleValue).register(meterRegistry);
+        Gauge.builder("checkouts", numberOfCheckouts, Integer::valueOf).register(meterRegistry);
     }
 }
